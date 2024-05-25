cb9784713129a359c6faa69dd81c53610ecba9d6
leonardopadilha <leonardopadilha28@gmail.com>
2024-05-25 05:56:40 -0300

    Adicionando rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const pessoas = require('./pessoaRoute.js');
+const categorias = require('./CategoriasRoute.js');
+const cursos = require('./CursoRoute.js');
 
 module.exports = app => {
   app.use(
     express.json(),
     pessoas,
+    categorias,
+    cursos
   );
 };
--- src/routes/pessoaRoute.js
+++ src/routes/pessoaRoute.js
@@ -1,12 +1,22 @@
 const { Router } = require('express');
 const PessoaController = require('../controllers/PessoaController.js');
 
+const MatriculaContoller = require('../controllers/MatriculaController.js');
+
 const pessoaController = new PessoaController();
+const matriculaController = new MatriculaContoller();
 
 const router = Router();
 
 router.get('/pessoas', (req, res) => pessoaController.pegaTodos(req, res));
+router.get('/pessoas/:id', (req, res) => pessoaController.pegaUmId(req, res));
+
+router.post('/pessoas', (req, res) => pessoaController.criaNovo(req, res));
+router.post('/pessoas/:estudanteId/matriculas', (req, res) => MatriculaContoller.criaNovo(req, res));
+
 router.put('/pessoas/:id', (req, res) => pessoaController.atualiza(req, res));
+
+router.delete('/pessoas/:id', (req, res) => pessoaController.exclui(req, res));
 
 module.exports = router;
 
